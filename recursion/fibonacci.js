6617b8886e5ec3fbd06c3e1f7e21219a85671267
Zelalem A. Enyew <zelalem.antigegn12@gmail.com>
2022-08-22 23:09:18 +0300

    implementing bottom  up and top down aproach for fibonachi series

--- recursion/fibonacci.js
+++ recursion/fibonacci.js
@@ -1,22 +1,62 @@
-function fibonacci(n){
+function recursive(n){
     if(n <= 1 )return n
-    return fibonacci(n - 1) + fibonacci(n - 2)
+    return recursive(n - 1) + recursive(n - 2)
 }
 
 
-function memorizedFibonacci(n){
-    const f  = Array(n + 1).fill(0);
-    f[1] = 1;
+function iterative(n){
+    let second = 0;
+    let first = 1;
+    let current;
+    for(let i = 2; i <= n; i++){
+       current = first + second;
+       second = first;
+       first = current;
+    }
+    return current;
+}
+
+let iCache = [];
+function memorizedrecursive(n){
+        if(n <= 1 )return n;
+        if(iCache[n] )return iCache[n];
+        return iCache[n] = memorizedrecursive(n - 1) + memorizedrecursive(n - 2)
+    }
+
+function memorizedIterative(n){ // Also called bottom_up approach
+    const cache = Array(n + 1).fill(-1);
+    cache[0] = 0;
+    cache[1] = 1;
 
     for(let i = 2; i <= n; i++){
-        f[i] = f[i - 1] + f[i - 2]
+        cache[i] = cache[i - 1] + cache[i - 2]
     }
-    return f[n]
+    return cache[n]
 }
 
+const num = 1000;
 
-console.time('test')
+// console.time('testR')
 
-console.log(memorizedFibonacci(50))
+// console.log(recursive(num))
 
-console.timeEnd('test')
+// console.timeEnd('testR')
+
+
+console.time('SimpleIterative')
+
+console.log(iterative(num))
+
+console.timeEnd('SimpleIterative')
+
+console.time('CachedRecursive')
+
+console.log(memorizedrecursive(num))
+
+console.timeEnd('CachedRecursive')
+
+console.time('CachedIterative')
+
+console.log(memorizedIterative(num))
+
+console.timeEnd('CachedIterative')
