1411556fc44581ff0017ee2f14c87941078b91f5
Zelalem A. Enyew <zelalem.antigegn12@gmail.com>
2022-08-30 23:08:17 +0300

    permutaion with deplicated

--- recursion/permutaion.js
+++ recursion/permutaion.js
@@ -15,5 +15,27 @@ function permutation(nums){
     return result;
 }
 
+function permuteUnique(nums) {
+    const result = [];
+    function _permuteUnique(index, nums){
+        if(index === nums.length){
+            result.push([...nums]);
+            return;
+        }
+        const hash = {};
+        for(let j = index ; j < nums.length; j++){
+            if(hash[nums[j]]) continue;
+            hash[nums[j]] = true;
+            [nums[index], nums[j]] = [nums[j], nums[index]];
+            _permuteUnique(index + 1, nums);
+              [nums[index], nums[j]] = [nums[j], nums[index]];
+        }
+    }
+    _permuteUnique(0, nums);
+    return result;
+}
 
-console.log(permutation(['a','c','b']))
+
+
+console.log(permutation(['a','c','b']));
+console.log(permuteUnique(['a','a','b']))
