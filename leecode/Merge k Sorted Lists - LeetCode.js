3b044e0bec9f5416a8eebb635bde24eaaf75be68
Zelalem A. Enyew <zelalem.antigegn12@gmail.com>
2022-09-21 22:25:57 +0300

    merging two sorted linked lists by combining one by one

--- leecode/Merge k Sorted Lists - LeetCode.js
+++ leecode/Merge k Sorted Lists - LeetCode.js
@@ -36,3 +36,60 @@ function mergeKLists(lists) {
    
    return head.next;
 };
+
+
+
+// Merge the two lists one by one;
+
+  function mergeTwoListsBrutFOrce(listOne, listTwo){
+    if(listOne  === null) return listTwo;
+    if(listTwo === null) return listOne;
+    
+    let curr = null;
+    
+    if(listOne.val <= listTwo.val){
+        curr = listOne;
+        listOne = listOne.next;
+    } else {
+        curr = listTwo;
+        listTwo = listTwo.next;
+    }
+    
+    const head = curr;
+    
+    while(listOne !== null && listTwo !== null){
+        if(listOne.val <= listTwo.val){
+            curr.next = listOne;
+            listOne = listOne.next;
+        } else {
+            curr.next = listTwo;
+            listTwo = listTwo.next;
+        }
+        curr = curr.next;
+    }
+    
+    if(listOne !== null) curr.next = listOne;
+    if(listTwo !== null) curr.next = listTwo;
+    
+    return head;
+    
+}
+
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+ function mergeKListsOneByONe(lists) {
+    const listsCount = lists.length;
+    if(listsCount === 0) return null;
+    if(listsCount === 1) return lists[0];
+    
+    let head = mergeTwoLists(lists[0], lists[1]);
+    
+    for(let i = 2; i < listsCount; i++){
+        head = mergeTwoLists(head, lists[i])
+    }
+    
+    
+    return head;
+};
