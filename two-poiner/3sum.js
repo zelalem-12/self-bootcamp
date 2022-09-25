0be314c71cccd5bba639e590b51b7c8fa95f38df
Zelalem A. Enyew <zelalem.antigegn12@gmail.com>
2022-09-26 02:33:22 +0300

    none zero triple sums

--- two-poiner/3sum.js
+++ two-poiner/3sum.js
@@ -36,3 +36,65 @@ function threeSum(nums) {
     
     return triplets;
 };
+
+
+
+function hashMap(first, second, third){
+    return [
+          `${first}${second}${third}`,
+          `${second}${first}${third}`,
+          `${third}${second}${first}`,
+          `${first}${third}${second}`,
+          `${second}${third}${first}`,
+          `${third}${first}${second}`,
+      ]     
+  }
+  
+  function searchPairSum(nums, tempTarget, triplets, leftPointer, originalTarget, hash){
+      let rightPointer = nums.length - 1;
+      
+      while(leftPointer, rightPointer){
+          const pairSum = nums[leftPointer] + nums[rightPointer];
+          if(tempTarget === pairSum){
+              const checks  = hashMap(originalTarget - tempTarget, nums[leftPointer], nums[rightPointer]);
+              if(checks.some(key =>hash[key])){
+                  leftPointer++;
+                  rightPointer--;
+                  while(leftPointer < rightPointer && nums[leftPointer] === nums[leftPointer - 1]) leftPointer++;
+                  while(leftPointer < rightPointer && nums[rightPointer] === nums[rightPointer + 1]) rightPointer--;  
+              } else {
+                  triplets.push([originalTarget - tempTarget, nums[leftPointer], nums[rightPointer]]);
+                  checks.forEach(check =>hash[check] = true)
+                  leftPointer++;
+                  rightPointer--;
+                  while(leftPointer < rightPointer && nums[leftPointer] === nums[leftPointer - 1]) leftPointer++;
+                  while(leftPointer < rightPointer && nums[rightPointer] === nums[rightPointer + 1]) rightPointer--;   
+              } 
+          } else if(tempTarget > pairSum) leftPointer++;
+          else rightPointer--;
+      }
+  }
+  
+  
+  /**
+   * @param {number[]} nums
+   * @param {number} target
+   * @return {number}
+   */
+  function threeSumNoneZere(nums, target) {
+      
+      nums.sort((a, b) => a - b)
+      
+      const triplets = [];
+      const hash = {}
+      
+      for(let i = 0; i < nums.length; i++){
+          if(i > 0 && nums[i] === nums[i - 1]) continue;
+          
+          const tempTarget = target - nums[i];
+          console.log(tempTarget)
+          // a + b + c = target,  a + b = target - c 
+          searchPairSum(nums, tempTarget, triplets, i + 1, target,hash);
+      }  
+      console.log(triplets)
+  };
